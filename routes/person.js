67d2e0e73807d7a166fbbd09b1e60771dab65216
Armagan Amcalar <armagan@amcalar.com>
2017-11-02 11:25:49 +0100

    Add person route and views

---
+++ routes/person.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+
+const PersonService = require('../services/person-service')
+
+router.get('/', async (req, res, next) => {
+    res.send(await PersonService.findAll())
+})
+
+router.get('/all', async (req, res, next) => {
+    const people = await PersonService.findAll()
+    res.render('person-list', {people})
+})
+
+router.get('/:id', async (req, res, next) => {
+    const person = await PersonService.find(req.params.id)
+
+    res.render('person-detail', {person})
+})
+
+router.post('/', async (req, res, next) => {
+    const person = await PersonService.add(req.body)
+
+    res.send(person)
+})
+
+router.delete('/:id', async (req, res, next) => {
+    await PersonService.del(req.params.id)
+
+    res.send('ok!')
+})
+
+module.exports = router
