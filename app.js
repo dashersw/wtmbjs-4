ac657eb6352cba7eb75ff2db1f5ca589d5ba1c9b
Armagan Amcalar <armagan@amcalar.com>
2017-11-23 12:38:49 +0100

    Break out app as a module to make it testable

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+require('./database-connection')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.set('view engine', 'pug')
+
+const person = require('./routes/person')
+
+app.use('/person', person)
+
+app.get('/', (req, res, next) => {
+    res.render('index')
+})
+
+module.exports = app
